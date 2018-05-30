eca6f302cc233764bd57f8a915aba0f6c36391d6
Caleb Blackwell <calebxblackwell@gmail.com>
2018-05-30 13:22:38 -0700

    css commit

--- src/components/ProgramContainer.js
+++ src/components/ProgramContainer.js
@@ -5,11 +5,9 @@ export const ProgramContainer = (props) =>  {
     return(
       <div>
         <ul>
-          <li>
-            {props.Name},
-            {props.Id},
-            {props.Description}
-          </li>
+            <h2>{props.Name}</h2>
+            <h3>{props.Id}</h3>
+            <h3>{props.Description}</h3>
         </ul>
       </div>
     )

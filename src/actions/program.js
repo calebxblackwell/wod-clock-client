4c25ee54f202de0d02c54a3ed860579e1fdf1f1a
Caleb Blackwell <calebxblackwell@gmail.com>
2018-06-06 10:38:55 -0700

    commit

--- src/actions/program.js
+++ src/actions/program.js
@@ -1,19 +1,24 @@
 //this is the ajax request for the workout information from wodify api
-const url = 'https://app.wodify.com/API/WODs_v1.aspx';
-const data = {apiKey: 'FvunGgKZG43ArkOBAs8KoFrT4', date: 'date',
-    location: 'Black Iron Gym', program: 'CrossFit', type: 'json'};
+//const url = 'https://app.wodify.com/API/WODs_v1.aspx?apiKey=FvunGgKZG43ArkOBAs8KoFrT4&type=json&date=date&program=CrossFit&location=BlackIronGym';
+const url = 'https://app.wodify.com/API/Programs_v1.aspx?apiKey=FvunGgKZG43ArkOBAs8KoFrT4&type=json&date=date&program=CrossFit&location=BlackIronGym';
 
 export const fetchPrograms = () => {
   return (dispatch) => {
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers:{
-        'Content-Type': 'application/json'
-  }
-})
-    .then(response => response.json())
-    .then(json => console.log(json))
-  .catch((ex) => console.log('parsing failed', ex))
-  }
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+      });
+//     fetch(url, {
+//       method: 'GET',
+//       headers:{
+//         'Content-Type': 'application/json'
+//   }
+// })
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+//   .catch((ex) => console.log('parsing failed', ex))
+   }
 }

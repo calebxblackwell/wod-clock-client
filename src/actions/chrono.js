0a47733a6df12f7e7caf6dddbbc0a05ae69f1f67
Caleb Blackwell <calebxblackwell@gmail.com>
2018-06-06 08:57:09 -0700

    commit

--- src/actions/chrono.js
+++ src/actions/chrono.js
@@ -1,7 +1,7 @@
 export const saveUserTime = (secondsElapsed) => {
     return () => {
       console.log('return saveUserTime');
-      fetch('http://localhost:8080/time' , {
+      fetch('http://localhost:8080/api/time/time' , {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify

1fd84a23e1b494ff10684e9427754cbd7b26d2d6
Caleb Blackwell <calebxblackwell@gmail.com>
2018-06-08 10:35:29 -0700

    commit

--- src/helpers/history.js
+++
@@ -1,7 +0,0 @@
-//enable redirecting users from outside React components, for example
-//from the user actions after successful login.
-
-
-import { createBrowserHistory } from 'history';
-
-export const history = createBrowserHistory();
